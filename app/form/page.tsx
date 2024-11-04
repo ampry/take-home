c585a75745af55c047fd1fc7cce00713bb3ac73c
Bridger Tower <bridgertower@gmail.com>
2024-11-04 10:15:27 -0700

    wip

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -5,11 +5,15 @@ export default function Page() {
   return (
     <Section>
       <Container>
-        <h1>Wow! a form.</h1>
-        <h3 className="text-muted-foreground my-0">
-          Submit the form to see the what happens.
-        </h3>
-        <div className="mt-4 p-6 border rounded-lg bg-accent/30">
+        {/* Header */}
+        <div className="text-center max-w-md mx-auto">
+          <h1>Wow! a form.</h1>
+          <h3 className="text-muted-foreground my-0">
+            Submit the form to see the what happens.
+          </h3>
+        </div>
+        {/* Form */}
+        <div className="mt-4 p-6 border rounded-lg bg-accent/30 max-w-md mx-auto">
           <InputForm />
         </div>
       </Container>
